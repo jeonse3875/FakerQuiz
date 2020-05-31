32fc2d2a8ba5e510039bf53c9c98c2ce87e25304
jeonse3875 <jeonse3875@khu.ac.kr>
2020-05-31 18:42:07 +0900

    테스트: axios와 cheerio로 롤인벤 크롤링 테스트

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getHTML = async () => {
+    try {
+        return await axios.get('http://lol.inven.co.kr/dataninfo/proteam/progamer.php?code=135');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getHTML()
+    .then(html => {
+        const $ = cheerio.load(html.data);
+        var title = $("h2.block.name").text();
+
+        return title;
+    })
+    .then(res => console.log(res));
+
 app.get('/', (req, res) => {
     res.send('Express Test');
 });
