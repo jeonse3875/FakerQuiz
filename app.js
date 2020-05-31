b5c872898078095f86e5da4ee8f1493ede15ddec
jeonse3875 <jeonse3875@khu.ac.kr>
2020-05-31 20:38:19 +0900

    기능: LCK 통산 전적 크롤링

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const port = 80;
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+const url = 'http://lol.inven.co.kr/dataninfo/proteam/progamer.php?code=135';
+
+var fakerData = {};
 
 const getHTML = async () => {
     try {
-        return await axios.get('http://lol.inven.co.kr/dataninfo/proteam/progamer.php?code=135');
+        return await axios.get(url);
     } catch (error) {
         console.error(error);
     }
@@ -16,9 +19,23 @@ const getHTML = async () => {
 getHTML()
     .then(html => {
         const $ = cheerio.load(html.data);
-        var title = $("h2.block.name").text();
 
-        return title;
+        // LCK 통산 전적
+        var tr = $('table.table.log_list.log01 tbody').children();
+        var td = tr.eq(0).children();
+        fakerData['lCK'] = {
+            totalGamePlay : td.eq(1).text(),
+            totalWin : td.eq(2).text(),
+            totalLose : td.eq(3).text(),
+            winRate : td.eq(4).text(),
+            killPerMatch : td.eq(5).text(),
+            deathPerMatch : td.eq(6).text(),
+            assistPerMatch : td.eq(7).text(),
+            kDA : td.eq(8).text(),
+            killParticipation : td.eq(9).text()
+        };
+
+        return fakerData;
     })
     .then(res => console.log(res));
 
