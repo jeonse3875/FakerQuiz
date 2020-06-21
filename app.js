9009147a747913d57791dd0d10e63ef3285aa96c
jeonse3875 <jeonse3875@khu.ac.kr>
2020-06-22 02:01:49 +0900

    FakerQuiz v1.1

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const domain = "";// ***Your Domain***
 const sslport = 23023;
 const bodyParser = require('body-parser');
 
-const version = '1.0';
+const version = '1.1';
 const dataFileName = 'data.json';
 
 var fakerData = {};
