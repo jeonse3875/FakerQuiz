3118027268a707584064d8ab01b268ff423bd6b6
Ubuntu <ubuntu@ip-172-31-37-109.ap-northeast-2.compute.internal>
2020-06-22 07:52:49 +0000

    FakerQuiz v1.3

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const domain = "";// ***Your Domain***
 const sslport = 23023;
 const bodyParser = require('body-parser');
 
-const version = '1.22';
+const version = '1.3';
 const dataFileName = 'data.json';
 
 var fakerData = {};
